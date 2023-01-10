73c362d6769284d958fd78e03a352b8187aed881
dmytromayor <mayor4090@gmail.com>
2023-01-10 21:55:58 +0200

    Update database component interface

--- felix/core/engine_components/database_components/interfaces.py
+++ felix/core/engine_components/database_components/interfaces.py
@@ -1,6 +1,29 @@
+import typing as t
+from abc import abstractmethod
+
 from objects.engine import IEngineComponent
 
 class IDatabaseEngineComponent(IEngineComponent):
     def __init__(self) -> None:
         super().__init__()
-        
+    
+    @abstractmethod
+    def save(self, table_name: str, data: t.Any) -> int:
+        pass
+
+    @abstractmethod
+    def load(self, table_name: str, object_id: int) -> t.Any:
+        pass
+
+
+class IJsonDatabaseEngineComponent(IEngineComponent):
+    def __init__(self) -> None:
+        super().__init__()
+
+    @abstractmethod
+    def save(self, table_name: str, data: t.Dict) -> int:
+        pass
+
+    @abstractmethod
+    def load(self, table_name: str, object_id: int) -> t.Optional[t.Dict]:
+        pass
