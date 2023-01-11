46ba81258e9de547b53c54f01602fd08c3c1c4a9
dmytromayor <mayor4090@gmail.com>
2023-01-11 14:05:14 +0200

    Update engine interfaces

--- felix/core/objects/engine/interfaces.py
+++ felix/core/objects/engine/interfaces.py
@@ -8,16 +8,3 @@ class IEngineComponent(Observable):
     @abstractmethod
     def update_state(self, time_delta: float) -> None:
         pass
-
-class IEngine(Observable):
-    @abstractmethod
-    def register_component(self, interface_type: type, object: IEngineComponent) -> None:
-        pass
-
-    @abstractmethod
-    def get_component(self, interface_type: type) -> t.Optional['IEngineComponent']:
-        pass
-
-    @abstractmethod
-    def update_state(self, time_delta: float) -> None:
-        pass
