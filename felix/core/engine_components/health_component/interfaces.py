1a3e8026cebbd9cb281cb8d0bc41970aad2834c2
dmytromayor <mayor4090@gmail.com>
2023-01-11 17:39:46 +0200

    Add interface for the health class

--- felix/core/engine_components/health_component/interfaces.py
+++ felix/core/engine_components/health_component/interfaces.py
@@ -4,6 +4,28 @@ from objects.unique_object import ILinkedUniqueObject
 from objects.engine import IEngineComponent
 from object_component import IObjectEngineComponent
 
+class IHealth(ILinkedUniqueObject):
+    @property
+    @abstractmethod
+    def value(self) -> int:
+        pass
+
+    @value.setter
+    @abstractmethod
+    def value(self, new_value: int) -> None:
+        pass
+    
+    @property
+    @abstractmethod
+    def max_health(self) -> int:
+        pass
+    
+    @max_health.setter
+    @abstractmethod
+    def max_health(self, new_value: int) -> None:
+        pass
+
+
 class IHealthEngineComponent(IEngineComponent):
     @abstractmethod
     def register_object_component(self, object_engine_component: IObjectEngineComponent) -> None:
