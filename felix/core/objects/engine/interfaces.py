8c1dbdb9c7d9417774393eb701b7cc06f4b54925
dmytromayor <mayor4090@gmail.com>
2023-01-11 14:11:14 +0200

    Add interface for engine factory

--- felix/core/objects/engine/interfaces.py
+++ felix/core/objects/engine/interfaces.py
@@ -8,3 +8,8 @@ class IEngineComponent(Observable):
     @abstractmethod
     def update_state(self, time_delta: float) -> None:
         pass
+
+class IEngineFactory(ABC):
+    @abstractmethod
+    def create(self) -> IEngineComponent:
+        pass
