f0287d76935cc72c29d2038292a5bca804a6eaed
dmytromayor <mayor4090@gmail.com>
2023-01-12 14:44:51 +0200

    Update init file of the pets component

--- felix/core/engine_components/pet_component/__init__.py
+++ felix/core/engine_components/pet_component/__init__.py
@@ -1,2 +1,2 @@
 from .interfaces import IPet , IPetFactory, IPetEngineComponent
-from .pets import Pet, PetEngineComponent, ChickenPetFactory
+from .pets import Pet, PetEngineComponent, ChickenPetFactory, DefaultPetFactoryBase, DefaultDBPetFactory, DefaultPetFactory
