411c0f423f06280e6afaba5e0ba0ca80e1f3328d
dmytromayor <mayor4090@gmail.com>
2023-01-12 14:43:39 +0200

    Update architecture of the pets component

--- felix/core/engine_components/pet_component/pets.py
+++ felix/core/engine_components/pet_component/pets.py
@@ -1,12 +1,17 @@
 import typing as t
+from sqlalchemy import Column, String, Integer, ForeignKey
 
 from .interfaces import IPet, IPetFactory, IPetEngineComponent
 from ...database import Base
 from ...general.unique_object import LinkedUniqueObjectMixin, IUniqueIDGenerator
 from ...tools import Observable, IDependencyInjector
 
-# class DatabasePet(Base):
-#     pass
+class DBPetModel(Base):
+    __tablename__ = "pets"
+
+    id = Column(Integer, primary_key=True)
+    owner_id = Column(Integer, index=True)
+    type = Column(String)
 
 class Pet(IPet, LinkedUniqueObjectMixin):
     def __init__(self, id: int, owner_id: int, type: str) -> None:
@@ -17,17 +22,42 @@ class Pet(IPet, LinkedUniqueObjectMixin):
     def type(self) -> str:
         return self.__type
 
-class ChickenPetFactory(IPetFactory):
-    def __init__(self, di_container: IDependencyInjector) -> None:
+class DBPet(IPet, LinkedUniqueObjectMixin):
+    def __init__(self, id: int, owner_id: int, type: str) -> None:
+        super().__init__(id=id, owner_id=owner_id)
+        self.__db_instance = DBPetModel(id=id, owner_id=owner_id, type=type)
+    
+    @property
+    def type(self) -> str:
+        return str(self.__db_instance.type)
+
+class DefaultPetFactoryBase(IPetFactory):
+    def __init__(self, di_container: IDependencyInjector, default_type: str) -> None:
         super().__init__()
         id_generator: t.Optional[IUniqueIDGenerator] = di_container.get_singleton(IUniqueIDGenerator)
         if id_generator == None:
             raise ValueError("Can't get id generator from DI container")
 
-        self.__id_generator: IUniqueIDGenerator = id_generator
+        self._id_generator: IUniqueIDGenerator = id_generator
+        self._default_type: str = default_type
+
+class DefaultPetFactory(DefaultPetFactoryBase):
+    def __init__(self, di_container: IDependencyInjector, default_type: str) -> None:
+        super().__init__(di_container, default_type)
 
     def create(self, owner_id: int) -> IPet:
-        return Pet(self.__id_generator.create_id(), owner_id, "chicken")
+        return Pet(self._id_generator.create_id(), owner_id, self._default_type)
+
+class DefaultDBPetFactory(DefaultPetFactoryBase):
+    def __init__(self, di_container: IDependencyInjector, default_type: str) -> None:
+        super().__init__(di_container, default_type)
+
+    def create(self, owner_id: int) -> IPet:
+        return DBPet(self._id_generator.create_id(), owner_id, self._default_type)
+
+class ChickenPetFactory(DefaultPetFactory):
+    def __init__(self, di_container: IDependencyInjector) -> None:
+        super().__init__(di_container, "chicken")
 
 class PetEngineComponent(IPetEngineComponent, Observable):
     def __init__(self, pet_factory: IPetFactory) -> None:
