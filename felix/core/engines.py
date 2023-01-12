6ba11263f8cd22062ffbb8b55cbfd5885ac90ad2
dmytromayor <mayor4090@gmail.com>
2023-01-12 15:12:51 +0200

    Update pets engine with EngineRunMixin

--- felix/core/engines.py
+++ felix/core/engines.py
@@ -1,11 +1,11 @@
-from general.engine import IEngine
+from general.engine import IEngine, EngineRunMixin
 from general.unique_object import UUID1, IUniqueIDGenerator
 from tools.dependency_injector import IDependencyInjector, DependencyInjector
 from engine_components.pet_component import PetEngineComponent, IPetEngineComponent, ChickenPetFactory
 
-class PetsEngine(IEngine):
+class PetsEngine(EngineRunMixin, IEngine):
     def __init__(self) -> None:
-        super().__init__()
+        super().__init__(tickrate=20)
 
         self.__di_conrainer: IDependencyInjector = DependencyInjector()
         uuid1_generator: IUniqueIDGenerator = UUID1()
@@ -14,11 +14,8 @@ class PetsEngine(IEngine):
         self.__pet_component = PetEngineComponent(ChickenPetFactory(self.__di_conrainer))
         self.__di_conrainer.register_singleton(IPetEngineComponent, self.__pet_component)
     
-    def __update_state(self, time_delta: float) -> None:
+    def update_state(self, time_delta: float) -> None:
         self.__pet_component.update_state(time_delta)
-
-    def run(self) -> None:
-        return super().run()
 
     def get_di_container(self) -> IDependencyInjector:
         return self.__di_conrainer
