f1b06db3121b90efc6b488c427e0fd1f4bfee929
dmytromayor <mayor4090@gmail.com>
2023-01-12 15:17:00 +0200

    Update telegram handlers

--- felix/telegram/bot/handlers.py
+++ felix/telegram/bot/handlers.py
@@ -3,4 +3,8 @@ from . import tbot
 
 @tbot.message_handler(commands=["start"])
 def send_start_message(message: types.Message) -> None:
+    tbot.reply_to(message, "Example")
+
+@tbot.message_handler(commands=["/create_pet"])
+def create_pet_command(message: types.Message) -> None:
     pass
