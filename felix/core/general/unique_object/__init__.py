1288928910cb939c12a9e96cfe3d59258e467847
dmytromayor <mayor4090@gmail.com>
2023-01-13 13:04:13 +0200

    Fix init file of the unique_object module

--- felix/core/general/unique_object/__init__.py
+++ felix/core/general/unique_object/__init__.py
@@ -1,3 +1,3 @@
-from .interfaces import IUniqueIDGenerator, ILinkedUniqueObject
+from .interfaces import IUniqueIDGenerator, ILinkedUniqueObject, IUniqueObject
 from .generators import UUID1
 from .mixins import LinkedUniqueObjectMixin
