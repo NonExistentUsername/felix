f9a31d979d49130e1c683a8baca976b910333b69
dmytromayor <mayor4090@gmail.com>
2023-01-13 15:25:02 +0200

    Fix creating new pet

--- felix/telegram/bot/controller.py
+++ felix/telegram/bot/controller.py
@@ -7,6 +7,9 @@ from .bot import tbot
 from .events import BotCommandEvent
 
 from ...core.engine_components.pet_component import IPetEngineComponent
+from ...core.engine_components.telegram_components.chat_manager import (
+    ITelegramChatManager,
+)
 
 
 class TelegramController(IController, IObserver):
@@ -21,12 +24,23 @@ class TelegramController(IController, IObserver):
 
         self.__pet_engine_component: IPetEngineComponent = pet_engine_component
 
+        telegram_chat_manager: t.Optional[
+            ITelegramChatManager
+        ] = engine_di_controller.get_singleton(ITelegramChatManager)
+
+        if telegram_chat_manager is None:
+            raise ValueError("Can't get engine component for telegram chats")
+
+        self.__telegram_chat_manager: ITelegramChatManager = telegram_chat_manager
+
     def notify(self, event: IEvent) -> None:
         if not isinstance(event, BotCommandEvent):
             return
 
         if event.command == "create_pet":
-            self.__pet_engine_component.create_pet(123)
+            self.__pet_engine_component.create_pet(
+                self.__telegram_chat_manager.get_chat(event.kwargs["chat_id"]).get_id()
+            )
 
     def start(self) -> None:
         tbot.infinity_polling()
