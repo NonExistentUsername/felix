cb5558cfc917df8df8e01307dac3b58f661aabe2
dmytromayor <mayor4090@gmail.com>
2023-01-15 13:01:02 +0200

    Add handlers for set_pet_name and pet commands

--- felix/telegram/handlers.py
+++ felix/telegram/handlers.py
@@ -18,3 +18,19 @@ def create_pet_command(message: types.Message) -> None:
     command_observable_component.notify(
         BotCommandEvent("create_pet", chat_id=message.chat.id)
     )
+
+
+@tbot.message_handler(commands=["set_pet_name"])
+def set_pet_name_command(message: types.Message) -> None:
+    command_observable_component.notify(
+        BotCommandEvent(
+            "set_pet_name", chat_id=message.chat.id, new_name=str(message.text)[13:]
+        )
+    )
+
+
+@tbot.message_handler(commands=["pet"])
+def get_pet_command(message: types.Message) -> None:
+    command_observable_component.notify(
+        BotCommandEvent("get_pet", chat_id=message.chat.id)
+    )
