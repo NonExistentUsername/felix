c78bad4951dfeb5c17c58998106fa6ac01fcfc98
dmytromayor <mayor4090@gmail.com>
2023-01-22 18:11:45 +0200

    Add ability to see name of a pet

--- felix/telegram/controllers/pets.py
+++ felix/telegram/controllers/pets.py
@@ -3,6 +3,7 @@ import os
 import typing as t
 
 from controller import IController
+from core.engine_components.hunger_component import IHunger, IHungerEngineComponent
 from core.engine_components.pet_component import IPet, IPetEngineComponent
 from core.engine_components.pet_customization_component import (
     IPetCustomization,
@@ -11,6 +12,10 @@ from core.engine_components.pet_customization_component import (
 from core.engine_components.telegram_components.chat_manager import (
     ITelegramChat,
     ITelegramChatManager,
+)
+from core.engine_components.vivacity_component import (
+    IVivacity,
+    IVivacityEngineComponent,
 )
 from core.tools import IDependencyInjector, IEvent, IObserver
 from telebot import types as tgt
@@ -57,6 +62,14 @@ class PetsController(IObserver):
         self.__logger: t.Optional[logging.Logger] = engine_di_container.get_singleton(
             logging.Logger
         )
+
+        self.__hunger_engine_component: t.Optional[
+            IHungerEngineComponent
+        ] = engine_di_container.get_singleton(IHungerEngineComponent)
+
+        self.__vivacity_engine_component: t.Optional[
+            IVivacityEngineComponent
+        ] = engine_di_container.get_singleton(IVivacityEngineComponent)
 
     def __get_or_create_chat(self, chat_id: int) -> ITelegramChat:
         chat_instance: t.Optional[
@@ -116,6 +129,32 @@ class PetsController(IObserver):
 
         pets_customization.name = new_name
 
+    def __render_pets_info__name(self, language_code: str, pet: IPet) -> str:
+        pet_customization: IPetCustomization = (
+            self.__cutomization_engine_component.get_pet_customization(pet.get_id())
+        )
+
+        return txt(language_code, "templates.pet_name").format(
+            name=pet_customization.name
+        )
+
+    def __render_pets_info(self, chat_instance: ITelegramChat) -> str:
+        pet: t.Optional[IPet] = self.__pet_engine_component.get_pet(
+            owner_id=chat_instance.get_id()
+        )
+
+        if not pet:
+            return ""
+
+        result: str = self.__render_pets_info__name(chat_instance.language_code, pet)
+
+        return result
+
+    def __get_pet_command(self, event: BotCommandEvent) -> None:
+        chat_instance: ITelegramChat = self.__get_or_create_chat(event.chat_id)
+
+        tbot.send_message(event.chat_id, self.__render_pets_info(chat_instance))
+
     def notify(self, event: IEvent) -> None:
         if not isinstance(event, BotCommandEvent):
             return
@@ -124,3 +163,5 @@ class PetsController(IObserver):
             self.__create_pet_command(event)
         elif event.command == "set_pet_name":
             self.__pet_set_name_command(event)
+        elif event.command == "get_pet":
+            self.__get_pet_command(event)
